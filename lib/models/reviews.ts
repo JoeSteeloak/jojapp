2464d265d5f68cba6324fd27e1a6580d45032727
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2025-03-03 09:57:15 +0100

    add bookId to reviewModel

--- lib/models/reviews.ts
+++ lib/models/reviews.ts
@@ -5,10 +5,11 @@ const ReviewSchema = new Schema(
         rating: { type: Number, min: 1, max: 5, required: true },
         comment: { type: String, required: true },
         user: { type: Schema.Types.ObjectId, ref: "User", required: true },
-     },
-     {
-         timestamps: true,
-     }
+        bookId: { type: String, required: true }
+    },
+    {
+        timestamps: true,
+    }
 );
 
 const Review = models.Review || model("Review", ReviewSchema);
