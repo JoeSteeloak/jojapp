ebf45cf1642d2a5e8e120d3a67327f6406ed2f2e
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2025-03-03 11:09:03 +0100

    add bookInterface

--- app/(subpages)/search/page.tsx
+++ app/(subpages)/search/page.tsx
@@ -1,5 +1,17 @@
 import React from "react";
 
+interface BookInterface {
+    _id: string;
+    title: string;
+    author: string;
+    publishedDate: string;
+    pageCount: number;
+    isbn: string;
+    bookCover: string;
+    description: string;
+    genre: string[];  
+}
+
 const Search = () => {
     return <div>Search</div>;
 };
