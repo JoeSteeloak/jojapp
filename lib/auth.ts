51ab665b22d57fa6e3278c6666ea74395dc3b85e
JoeSteeloak <141022479+JoeSteeloak@users.noreply.github.com>
2025-03-06 09:34:56 +0100

    update jwt token expiration

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const SECRET = process.env.JWT_SECRET || "supersecret"; // Använd en miljövariabel i produktion!
 
 export const generateToken = (userId: string) => {
-    return jwt.sign({ userId }, SECRET, { expiresIn: "7d" });
+    return jwt.sign({ userId }, SECRET, { expiresIn: "1h" });
 };
 
 export const verifyToken = (token: string) => {
